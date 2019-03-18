b8ddb9270476e35479843e893ccc9959c6df6793
codetr <manusia.tados@gmail.com>
2019-03-18 09:55:59 +0800

    update

---
+++ kode/Constants.java
@@ -0,0 +1,77 @@
+package com.kodetr.gamehagon;
+
+public class Constants {
+    static String baseurl = "https://codetr.herokuapp.com";
+
+    static String[] title = {
+            "Duck Hunt",
+            "Crappy Bird",
+            "Super Mario",
+            "The Pond",
+            "Pappu",
+            "2048",
+            "capten rogers",
+            "coil",
+            "d3shooter",
+            "hexgl",
+            "Square Off",
+            "Couch 2048",
+            "Branching Out",
+            "Hexahedral",
+            "Hextris",
+            "Monkey",
+            "Parity",
+            "Orbium",
+            "Sudoku",
+            "Zolmeister",
+            "Arturkot",
+    };
+
+    static String[] imageurl = {
+            "duckhunt.png",
+            "crappybird.png",
+            "supermario.png",
+            "ThePond.png",
+            "pappu.png",
+            "2048.png",
+            "captenrogers.png",
+            "coil.png",
+            "d3shooter.png",
+            "hexgl.png",
+            "squareoff.png",
+            "couch2048.png",
+            "branchingout.png",
+            "hexahedral.png",
+            "hextris.png",
+            "Monkey.png",
+            "parity.png",
+            "Orbium.png",
+            "sudoku.png",
+            "zolmeister.png",
+            "arturkot.png",
+    };
+
+    static String[] gameurl = {
+            "https://duckhuntjs.com/",
+            "http://www.varunpant.com/static/resources/CrappyBird/index.html",
+            "http://martindrapeau.github.io/backbone-game-engine/super-mario-bros/index.html",
+            "https://thepond.zolmeister.com/",
+            "http://khele.in/pappu-pakia/",
+            "https://play2048.co/",
+            "http://rogers.enclavegames.com/",
+            "https://lab.hakim.se/coil/",
+            "http://d3shooter.herokuapp.com/",
+            "http://hexgl.bkcore.com/play/",
+            "http://sqoff.com/",
+            "http://js13kgames.com/games/couch-2048/index.html",
+            "http://henryhoffman.com/branchingout/",
+            "http://matthewminer.com/hexahedral/",
+            "http://hextris.io/",
+            "http://antila.github.io/ludum-dare-28/",
+            "http://www.abefehr.com/parity/",
+            "http://bni.github.io/orbium/",
+            "http://baruchel.insomnia247.nl/sudoku-js/sudoku.html",
+            "https://zop.zolmeister.com/",
+            "https://the-house.arturkot.pl/",
+    };
+}
